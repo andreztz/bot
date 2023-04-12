b580490eb7e9db2959f6d51a724356a83ab3d7f3
André P. Santos <andreztz@gmail.com>
2023-04-12 07:10:53 -0300

    Add mpv command
    Added a new command to the bot that allows it to control an mpv instance.
    The command can be triggered by sending the message !mpv <path-to-file>
    to the bot. The implementation of the command includes using asyncio to
    run subprocesses and a socket connection to communicate with the mpv
    instance.

--- bot.py
+++ bot.py
@@ -1,19 +1,101 @@
+import asyncio
+import re
+import socket
 import slixmpp
 
+import psutil
 
-class MyBot(slixmpp.ClientXMPP):
+
+COMMAND_REGEX = re.compile(r"^!(\w+)\s*(.*)$")
+COMMANDS = {}
+
+
+def command(name):
+    def decorator(func):
+        COMMANDS[name] = func
+    return decorator
+
+async def is_mpv_running():
+    result = await asyncio.to_thread(_check_mpv_running)
+    return result
+
+
+def _check_mpv_running():
+    for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
+        if proc.info['name'] == 'mpv' and '--player-operation-mode=pseudo-gui' in proc.info['cmdline']:
+            return True
+    return False
+
+
+async def async_run(command):
+    process = await asyncio.create_subprocess_shell(
+        command, 
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    print(stdout.decode(), stderr.decode())
+
+
+@command("mpv")
+async def mpv_command(bot, message, *args, **kwargs):
+    is_running = await is_mpv_running()
+    if not is_running:
+        command = f'mpv --player-operation-mode=pseudo-gui {args[0]}'
+        asyncio.create_task(async_run(command))
+        bot.send_message(message["from"], "Iniciando")
+        return
+    else:
+        SOCKET_PATH = '/tmp/mpvsocket'
+
+        if args[0] == "fullscreen":
+            command = "set fullscreen yes\n"
+        else:
+            command = "loadfile {}\n".format(args[0])
+
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.connect(SOCKET_PATH)
+            s.sendall(bytes(command, encoding='utf-8'))
+            response = s.recv(1024)
+        print(response.decode('utf-8'))
+        bot.send_message(message["from"], "Executando comando no mpv")
+
+
+class Bot(slixmpp.ClientXMPP):
+
     def __init__(self, jid, password):
         slixmpp.ClientXMPP.__init__(self, jid, password)
         self.add_event_handler("session_start", self.start_session)
-        self.add_event_handler('message', self.message)
+        self.add_event_handler('message', self.message_handler)
+        self.commands = COMMANDS
 
     async def start_session(self, event):
         self.send_presence()
         self.get_roster()
 
+    async def message_handler(self, message):
+        if message['type'] in ('chat', 'normal'):
+            if match := COMMAND_REGEX.match(message["body"]):
+                name = match.group(1)
+                args = match.group(2).split()
 
-    async def message(self, msg):
-        print(msg)
+                func = self.commands[name]
+
+                if func:
+                    try:
+                        await func(self, message, *args)
+                    except Exception as exc:
+                        self.send_message(
+                            message['from'],
+                            f"Erro ao executar o comando '{name}': {exc}"
+                        )
+
+    def execute(self, command, args, msg):
+        try:
+            func = self.commands[command]
+            func(self, msg, *args)
+        except KeyError:
+            self.send_message(msg["from"], "Command not found.")
 
 
 if __name__ == '__main__':
@@ -23,30 +105,30 @@ if __name__ == '__main__':
     import ssl
 
     here = Path(__file__).parent
-    secrets = here.joinpath("secrets/users.json")
 
+    secrets = here.joinpath("secrets/users.json")
     users = json.loads(secrets.read_text())
 
     username = users[0]['username']
     password = users[0]['password']
-
 
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(levelname)-8s %(message)s'
     )
 
-    xmpp = MyBot(f'{username}@vmbox.lan/python-bot', password)
-    xmpp.register_plugin('xep_0030') # Service Discovery
-    xmpp.register_plugin('xep_0004') # Data Forms
-    xmpp.register_plugin('xep_0060') # PubSub
-    xmpp.register_plugin('xep_0199') # XMPP Ping
-    xmpp.register_plugin('xep_0199') # Ping
+    bot = Bot(f'{username}@vmbox.lan/python-bot', password)
+    bot.register_plugin('xep_0030') # Service Discovery
+    bot.register_plugin('xep_0004') # Data Forms
+    bot.register_plugin('xep_0060') # PubSub
+    bot.register_plugin('xep_0199') # XMPP Ping
+    bot.register_plugin('xep_0199') # Ping
+    bot.use_tls = True
 
-    xmpp.use_tls = True
     context = ssl.create_default_context()
     context.check_hostname = False
     context.verify_mode = ssl.CERT_NONE
-    xmpp.ssl_context = context
-    xmpp.connect(address=('vmbox.lan', 5222))
-    xmpp.process(forever=True)
+
+    bot.ssl_context = context
+    bot.connect(address=('vmbox.lan', 5222))
+    bot.process(forever=True)
